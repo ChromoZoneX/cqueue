d9c35c6a4d8ea8ee1fc3b30faf6118fbbccfe68b
Dhairyasarous <dp-1994@hotmail.com>
2014-07-18 20:57:10 -0400

    test 1

--- main.c
+++ main.c
@@ -2,5 +2,21 @@
 
 
 int main(int argc, char *argv[]){
+	cqueue * mq ;
+	int size;
+	size = 10;
+	cq_init(mq, size);
+
+	for(i = 0; i < size; i++){
+		cq_enqueue(mq, i);
+	}
+	cq_print(mq);
+	for(i = 0; i < 5; i++){
+		int ret;
+		cq_dequeue(mq, &ret)
+		printf ("dequeued item: %d\n",ret);
+	}
+	cq_print(mq);
+	
 	return 0;
 }
